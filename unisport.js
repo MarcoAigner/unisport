5371bc8fa6b7f3be1cf898920cde94025655b21a
Marco Aigner <m.f.aigner@googlemail.com>
2022-06-01 11:16:29 +0200

    node-schedule
    run the code based on node-schedule

--- unisport.js
+++ unisport.js
@@ -1,8 +1,15 @@
 import puppeteer from "puppeteer";
+import { scheduleJob } from "node-schedule";
 import "dotenv/config";
 
+/**
+ * In der .env-Datei werden die Zugangsdaten für den Unisport im Format
+ * E_MAIL="loremipsum"
+ * PW="loremipsum"
+ * abgelegt
+ */
+
 const LOGIN = "https://hochschulsport.uni-heidelberg.de/oa_oeff/login.php";
-const COURSES = "gr_info.php?krs=";
 
 async function login(page) {
   await page.type('input[name="email"]', process.env.E_MAIL);
@@ -18,16 +25,25 @@ async function bookTicket(page, courseShort) {
   await page.click('input[name="buchen"');
 }
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(LOGIN);
+/**
+ * Der String mit den Sternen beschreibt, wann der folgende Code ausgeführt werden soll
+ * Siehe auch https://crontab.guru/#1_7_*_*_3
+ * 1 7 * * 3 führt den Code beispielsweise immer Mittwochs um 07:01 aus
+ */
 
-  try {
-    await login(page);
-    await bookTicket(page, "ULT");
-    await browser.close();
-  } catch (error) {
-    console.log(error);
-  }
-})();
+scheduleJob('13 * * * *', function () {
+  (async () => {
+    console.log('Waiting for scheduled time')
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(LOGIN);
+  
+    try {
+      await login(page);
+      await bookTicket(page,"ULT");
+      await browser.close();
+    } catch (error) {
+      console.log(error);
+    }
+  })();
+});
